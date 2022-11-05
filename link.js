f300d0c28f3bc80d8f5f46a4c1e5356e3051684d
Haziq Zamri 3 <81473609+IHZAQSTORM33@users.noreply.github.com>
2022-11-05 14:07:00 +0800

    Create link.js

---
+++ link.js
@@ -0,0 +1,56 @@
+module.exports = {
+  yt:{ 
+    link: "https://www.youtube.com/channel/UCkv_Rb0zxQ71xeZ3UYmMvFw",
+    title: "My Youtube Channel",
+    desc: "Shortlink for IHZAQ youtube channel",
+    color: "#FF0000",
+    img: "https://codingwith.ihzaqstorm33bussiness.tk/favicon.png"
+  },
+  gg:{
+    link:"http://discord.gg/ndEftjM",
+    title: "Lightning Craft Server",
+    desc: "Come join us! in grave. yes this server is kinda dead.",
+    color: "#5865F2",
+    img: "https://cdn.discordapp.com/attachments/806523197196861521/945248186132471818/Discord-Logo-Color.png"
+  }, 
+  freerobux:{
+    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    title: "Free robux",
+    desc: "This is the only website that give you free robux. for real",
+    color: "#d6c20f",
+    img: "https://static.wikia.nocookie.net/roblox/images/3/34/Robux_2019_gold.svg/revision/latest/scale-to-width-down/100?cb=20191122105651"
+  },
+  ig:{
+    link:"https://instagram.com/ihzaqstorm33",
+    title: "Instagram Page",
+    desc: "My Instagram Page that full with dead memes",
+    color: "#e700f7",
+    img: "https://static.xx.fbcdn.net/rsrc.php/v3/yY/r/PfjFTePTXwO.png"
+  },
+  email:{
+    link:"mailto:info@IHZAQSTORM33Bussiness.tk?subject=BusinessEnquries&body=Hi Storm,",
+    title: "Enquiries(Business only)",
+    desc: "Only email me for business thing. copyright and stuff.",
+    color: "#FFFFFF",
+    img: "https://i.pinimg.com/originals/1b/22/fd/1b22fd858e93265a3a2fa2060cfc1219.jpg"
+  },
+  roblox:{
+    link:"https://roblox.com/users/1684676332",
+    title:"Storm Roblox Profile",
+    desc:"IHZAQSTORM33's Roblox Profile",
+    color: "#FFFFFF",
+    img: "https://t6.rbxcdn.com/c5d32462c874cbf3a34fb6edd5b7c048"
+  }, 
+  github: {
+    title:"Haziq Zamri's Github Profile", 
+    link:"https://github.com/IHZAQSTORM33", desc:"Developer Profile for Stormy", 
+    color:"#000000", 
+    img:"https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+  },
+  linkname: {
+    title:"Title", 
+    link:"https://example.com",
+    color:"#Hex Color Code here", 
+    img:"Image Link" 
+  } 
+} 
